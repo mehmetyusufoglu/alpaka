faa0b55d7463773955833827b0eadf180dbb3972
mehmet yusufoglu <mehmetyusufoglu01@gmail.com>
2025-01-21 15:55:07 +0100

    only CPU both examples good

--- example/sumOfSquaresSIMD/src/simd_library.hpp
+++ example/sumOfSquaresSIMD/src/simd_library.hpp
@@ -1,136 +1,74 @@
-#ifndef PORTABLE_SIMD_HPP
-#define PORTABLE_SIMD_HPP
+#ifndef SIMD_LIBRARY_HPP
+#define SIMD_LIBRARY_HPP
 
-#include <alpaka/alpaka.hpp>
-#ifdef __CUDACC__
-#include <cuda_runtime.h>
-#else
 #include <experimental/simd>
-#endif
-#include <type_traits>
-#include <numeric>
+#include <cstddef>
 
-namespace detail {
-    // Helper type trait to determine if the Acc type corresponds to CUDA.
-    template <typename Acc>
-    struct IsCudaAcc {
-        static constexpr bool value = std::is_same_v<typename alpaka::trait::AccToTag<Acc>::type, alpaka::TagGpuCudaRt>;
-    };
-} // namespace detail
+namespace stdx = std::experimental;
 
-template <typename T, typename Acc>
+template <typename T>
 class PortableSimd {
-private:
-    using SimdType = std::conditional_t<
-        detail::IsCudaAcc<Acc>::value,
-        T,                              // For CUDA, PortableSimd<T> is just T
-#ifndef __CUDACC__
-        std::experimental::simd<T>>;    // For CPU, use std::simd
-#else
-        T>;                             // Fallback for CUDA
-#endif
-
-    SimdType data;
+    stdx::simd<T> data;
 
 public:
-    // Default constructor
-    ALPAKA_FN_ACC PortableSimd() {
-        if constexpr (detail::IsCudaAcc<Acc>::value) {
-            data = T{}; // Scalar initialization to 0
-        } else {
-#ifndef __CUDACC__
-            data = std::experimental::simd<T>(); // Zero-initialize SIMD register
-#endif
-        }
+    static constexpr size_t size() {
+        return stdx::simd<T>::size();
+    }
+    void load(const T* ptr) {
+        data = stdx::simd<T>(ptr, stdx::element_aligned);
     }
 
-           // Constructor to initialize all elements to a scalar value
-    ALPAKA_FN_ACC explicit PortableSimd(T scalar) {
-        if constexpr (detail::IsCudaAcc<Acc>::value) {
-            data = scalar; // Scalar initialization
-        } else {
-#ifndef __CUDACC__
-            data = std::experimental::simd<T>(scalar); // SIMD register initialization
-#endif
+           // Assignment operator
+    PortableSimd& operator=(const PortableSimd& other) {
+        if (this != &other) {
+            data = other.data;
         }
+        return *this;
     }
 
-           // Get the SIMD width
-    ALPAKA_FN_ACC static constexpr size_t size() {
-        if constexpr (detail::IsCudaAcc<Acc>::value) {
-            return 1; // Scalar for CUDA
-        } else {
-#ifndef __CUDACC__
-            return std::experimental::simd<T>::size();
-#else
-            return 1; // Fallback for CUDA
-#endif
-        }
-    }
-
-           // Load from memory
-    ALPAKA_FN_ACC void load(const T* ptr) {
-        if constexpr (detail::IsCudaAcc<Acc>::value) {
-            data = *ptr; // Scalar load
-        } else {
-#ifndef __CUDACC__
-            data = std::experimental::simd<T>(ptr, std::experimental::element_aligned);
-#endif
-        }
-    }
-
-           // Store to memory
-    ALPAKA_FN_ACC void store(T* ptr) const {
-        if constexpr (detail::IsCudaAcc<Acc>::value) {
-            *ptr = data; // Scalar store
-        } else {
-#ifndef __CUDACC__
-            data.copy_to(ptr, std::experimental::element_aligned);
-#endif
-        }
+           // Operator := for loading data
+    PortableSimd& operator==(const T* ptr) {
+        data = stdx::simd<T>(ptr, stdx::element_aligned);
+        return *this;
     }
 
            // Perform addition
-    ALPAKA_FN_ACC PortableSimd operator+(const PortableSimd& other) const {
+    PortableSimd operator+(const PortableSimd& other) const {
         PortableSimd result;
-        if constexpr (detail::IsCudaAcc<Acc>::value) {
-            result.data = data + other.data; // Scalar addition
-        } else {
-#ifndef __CUDACC__
-            result.data = data + other.data; // SIMD addition
-#endif
-        }
+        result.data = data + other.data;
         return result;
     }
 
            // Perform multiplication
-    ALPAKA_FN_ACC PortableSimd operator*(const PortableSimd& other) const {
+    PortableSimd operator*(const PortableSimd& other) const {
         PortableSimd result;
-        if constexpr (detail::IsCudaAcc<Acc>::value) {
-            result.data = data * other.data; // Scalar multiplication
-        } else {
-#ifndef __CUDACC__
-            result.data = data * other.data; // SIMD multiplication
-#endif
+        result.data = data * other.data;
+        return result;
+    }
+
+           // Store the data back to memory
+    void store(T* ptr) const {
+        data.copy_to(ptr, stdx::element_aligned);
+    }
+
+           // Compute the sum of all elements in the SIMD object
+    T sum() const {
+        T result = 0;
+        for (size_t i = 0; i < data.size(); ++i) {
+            result += data[i];
         }
         return result;
     }
 
-           // Compute the sum of all elements in the SIMD object
-    ALPAKA_FN_ACC T sum() const {
-        if constexpr (detail::IsCudaAcc<Acc>::value) {
-            return data; // Scalar sum
-        } else {
-#ifndef __CUDACC__
-             T sum{};
-            for(int i=0;i<size();i++)
-            sum+=data[i];
-            return sum;
-#else
-            return data; // Fallback for CUDA
-#endif
+           // Apply a custom operation using a functor
+    template <typename Functor>
+    PortableSimd apply(const PortableSimd& other, Functor func) const {
+        PortableSimd result;
+        for (size_t i = 0; i < data.size(); ++i) {
+            result.data[i] = func(data[i], other.data[i]);
         }
+        return result;
     }
 };
 
-#endif // PORTABLE_SIMD_HPP
+#endif // SIMD_LIBRARY_HPP
