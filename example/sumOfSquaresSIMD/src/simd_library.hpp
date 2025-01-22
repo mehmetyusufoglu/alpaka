ae6ed7ff43d535b2ab05c5334442371fb0d0ca60
mehmet yusufoglu <mehmetyusufoglu01@gmail.com>
2025-01-22 15:36:32 +0100

    compiles for gpu and CPU seperately the sumOfSquares and its lib

--- example/sumOfSquaresSIMD/src/simd_library.hpp
+++ example/sumOfSquaresSIMD/src/simd_library.hpp
@@ -1,57 +1,53 @@
 #ifndef SIMD_LIBRARY_HPP
 #define SIMD_LIBRARY_HPP
 
-#include <experimental/simd>
 #include <cstddef>
 
+#if defined(ALPAKA_ACC_GPU_CUDA_ENABLED)
+#include <cuda_runtime.h>
+#endif
+
+// CPU SIMD implementation
+#if defined(ALPAKA_ACC_CPU_B_SEQ_T_SEQ_ENABLED)
+#include <experimental/simd>
 namespace stdx = std::experimental;
 
 template <typename T>
-class PortableSimd {
+class PortableSimdCpu {
     stdx::simd<T> data;
 
 public:
     static constexpr size_t size() {
         return stdx::simd<T>::size();
     }
+
     void load(const T* ptr) {
         data = stdx::simd<T>(ptr, stdx::element_aligned);
     }
 
-           // Assignment operator
-    PortableSimd& operator=(const PortableSimd& other) {
+    PortableSimdCpu& operator=(const PortableSimdCpu& other) {
         if (this != &other) {
             data = other.data;
         }
         return *this;
     }
 
-           // Operator := for loading data
-    PortableSimd& operator==(const T* ptr) {
-        data = stdx::simd<T>(ptr, stdx::element_aligned);
-        return *this;
-    }
-
-           // Perform addition
-    PortableSimd operator+(const PortableSimd& other) const {
-        PortableSimd result;
+    PortableSimdCpu operator+(const PortableSimdCpu& other) const {
+        PortableSimdCpu result;
         result.data = data + other.data;
         return result;
     }
 
-           // Perform multiplication
-    PortableSimd operator*(const PortableSimd& other) const {
-        PortableSimd result;
+    PortableSimdCpu operator*(const PortableSimdCpu& other) const {
+        PortableSimdCpu result;
         result.data = data * other.data;
         return result;
     }
 
-           // Store the data back to memory
     void store(T* ptr) const {
         data.copy_to(ptr, stdx::element_aligned);
     }
 
-           // Compute the sum of all elements in the SIMD object
     T sum() const {
         T result = 0;
         for (size_t i = 0; i < data.size(); ++i) {
@@ -59,16 +55,51 @@ public:
         }
         return result;
     }
+};
+#endif // ALPAKA_ACC_CPU_B_SEQ_T_SEQ_ENABLED
 
-           // Apply a custom operation using a functor
-    template <typename Functor>
-    PortableSimd apply(const PortableSimd& other, Functor func) const {
-        PortableSimd result;
-        for (size_t i = 0; i < data.size(); ++i) {
-            result.data[i] = func(data[i], other.data[i]);
+// CUDA SIMD implementation
+#if defined(ALPAKA_ACC_GPU_CUDA_ENABLED)
+template <typename T>
+class PortableSimdCuda {
+    T data;
+
+public:
+    ALPAKA_FN_ACC static constexpr size_t size() {
+        return 1; // Scalar for CUDA
+    }
+
+    ALPAKA_FN_ACC void load(const T* ptr) {
+        data = *ptr;
+    }
+
+    ALPAKA_FN_ACC PortableSimdCuda& operator=(const PortableSimdCuda& other) {
+        if (this != &other) {
+            data = other.data;
         }
+        return *this;
+    }
+
+    ALPAKA_FN_ACC PortableSimdCuda operator+(const PortableSimdCuda& other) const {
+        PortableSimdCuda result;
+        result.data = data + other.data;
         return result;
     }
+
+    ALPAKA_FN_ACC PortableSimdCuda operator*(const PortableSimdCuda& other) const {
+        PortableSimdCuda result;
+        result.data = data * other.data;
+        return result;
+    }
+
+    ALPAKA_FN_ACC void store(T* ptr) const {
+        *ptr = data;
+    }
+
+    ALPAKA_FN_ACC T sum() const {
+        return data; // Scalar sum
+    }
 };
+#endif // ALPAKA_ACC_GPU_CUDA_ENABLED
 
 #endif // SIMD_LIBRARY_HPP
